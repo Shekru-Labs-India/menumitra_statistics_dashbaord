5473a78e6f1bcd747f1c726e5dc9fd1d853c2d20
Rahul <rahulsomase03@gmail.com>
2025-03-13 16:33:25 +0530

    Changes done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,11 +8,11 @@ const Footer = () => {
   {/* Hello world */}
   <footer className="content-footer footer bg-footer-theme">
     <div className="container-xxl">
-      <div className="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
+      <div className="footer-container d-flex align-items-center justify-content-end py-4 flex-md-row flex-column">
         <div className="text-body mb-2 mb-md-0">
-          © MenuMitra, made with{" "}
+          © MenuMitra, Powered by{" "}
           <span className="text-danger">
-            <i className="tf-icons ri-heart-fill" />
+            
           </span>{" "}
           by{" "}
           <a
@@ -24,7 +24,7 @@ const Footer = () => {
           </a>
          
         </div>
-        <div className="d-none d-lg-inline-block">
+        {/* <div className="d-none d-lg-inline-block">
           <a
             href="https://themeselection.com/license/"
             className="footer-link me-4"
@@ -53,7 +53,7 @@ const Footer = () => {
           >
             Contact Us
           </a>
-        </div>
+        </div> */}
       </div>
     </div>
   </footer>
