12e5abbe6c72fe54ccd1e58ef03d5c88d5637541
Rahul <rahulsomase03@gmail.com>
2025-04-08 13:43:52 +0530

    Update API endpoints in Header and menuMitraConfig to use the new domain 'men4u.xyz'.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -80,7 +80,7 @@ function Header() {
         return;
       }
 
-      const response = await fetch('https://menusmitra.xyz/common_api/get_outlet_list', {
+      const response = await fetch('https://men4u.xyz/common_api/get_outlet_list', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
